e9724bc512127b8b5b1534e5b1e68367077f52d3
TuanAnhLUV <chantroiyeu1992@gmail.com>
2021-06-28 13:59:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em Yêu  à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'LOVE',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên ',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Yêu Anh',
+    reply: 'Em Yêu Anh Nhiều Lắm.......................................',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều ',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'LOVE',
+    messLink: 'https://www.facebook.com/tuananhktv92x' 
+}
